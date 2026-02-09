2a4fb710fa38b9ccdc103be328b61bd309776e9f
AbdalkaremDafees <karimhakimak24@gmail.com>
2026-02-09 21:02:56 +0300

    This is the two projects for #05 course

---
+++ 05. Algorithms & Problem Solving (L2)/05 Coures - Project 1.cpp
@@ -0,0 +1,167 @@
+﻿/* Write a small game program for Store-Paper-Scissor */
+
+#include <iostream>
+#include <Windows.h>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// Purpose: Defines the choices available in the game.
+enum enGameChoice {stone = 1, paper = 2, scissors = 3};
+
+// Purpose: Defines possible winners for a round or game.
+enum enWinner {Player = 1, Computer = 2, Draw = 3};
+
+// get rounds number from user
+short roundNumberFunc() {
+	short RNumber;
+	cout << "How many rounds 1 to 10 ? ";
+	do {
+		cin >> RNumber;
+	} while (RNumber > 10 || RNumber < 1);
+	return RNumber;
+}
+
+// get player choice (storn - paper - scissors)
+short playerChoiceFunc(short ctrRound) {
+	cout << "\nRound [" << ctrRound << "] begins: \n";
+
+	short PC;
+	cout << "\nYour Choice: [1]:Stone, [2]:Paper, [3]:Scissors ? ";
+	cin >> PC;
+	return PC;
+}
+
+// get a random number between (from - to) user defined
+short computerChoiceFunc(short from, short to) {
+	short CC = rand() % (to - from + 1) + from;
+	return CC;
+}
+
+// return who is the winner (player - computer - draw )
+short roundWinnerFunc(short playerChoice, short computerChoice, short &playerWinCtr, short &computerWinCtr, short &drawCtr) {
+	if(playerChoice == computerChoice) {
+		drawCtr++;
+		system("color 6F");
+		return enWinner::Draw;
+	}
+	else if ((playerChoice == 1 && computerChoice == 3) || (playerChoice == 2 && computerChoice == 1) || (playerChoice == 3 && computerChoice == 2)) {
+		playerWinCtr++;
+		system("color 2F");
+		return enWinner::Player;
+	}
+	else {
+		computerWinCtr++;
+		cout << '\a';
+		system("color 4F");
+		return enWinner::Computer;
+	}
+}
+
+// print what user choice
+void printPlayerChoice(short playerChoice) {
+	if(playerChoice == enGameChoice::stone)
+		cout << "\nPlayer Choice   : " << "Stone";
+	else if(playerChoice == enGameChoice::paper)
+		cout << "\nPlayer Choice   : " << "Paper";
+	else
+		cout << "\nPlayer Choice   : " << "scissors";
+}
+
+// print what computer choice
+void printComputerChoice(short computerChoice) {
+	if (computerChoice == enGameChoice::stone)
+		cout << "\nComputer Choice : " << "Stone";
+	else if (computerChoice == enGameChoice::paper)
+		cout << "\nComputer Choice : " << "Paper";
+	else
+		cout << "\nComputer Choice : " << "scissors";
+}
+
+// print who is winner in current round
+void printRoundWinner(short roundWinner) {
+	if (roundWinner == enWinner::Player)
+		cout << "\nRound Winner    : " << "[Player]";
+	else if (roundWinner == enWinner::Computer)
+		cout << "\nRound Winner    : " << "[Computer]";
+	else
+		cout << "\nRound Winner    : " << "[Draw]";
+}
+
+// print who is winner in all round
+void printRoundResult(short roundNumber, short playerChoice, short computerChoice, short roundWinner) {
+	cout << "\n_________________Round [" << roundNumber << "]_________________\n";
+
+	printPlayerChoice(playerChoice);
+	printComputerChoice(computerChoice);
+	printRoundWinner(roundWinner);
+
+	cout << "\n__________________________________________________________\n";
+}
+
+// style function
+void printHead() {
+	cout << "\n\t\t__________________________________________________________________________________________";
+	cout << "\n\t\t\t\t\t\t+++ G a m e  O v e r +++";
+	cout << "\n\t\t__________________________________________________________________________________________";
+	cout << "\n\t\t_____________________________________ [Game Results] _____________________________________";
+
+}
+
+// chek for who is the final winner in the game
+void checkFinalWinner(short playerWinCtr, short computerWinCtr, short drawCtr) {
+	if (playerWinCtr > computerWinCtr && playerWinCtr >= drawCtr) cout << "Player Win";
+	else if (computerWinCtr > playerWinCtr && computerWinCtr >= drawCtr) cout << "Computer Win";
+	else cout << "Draw";
+}
+
+// print all result
+void printFinalResult(short roundNumber, short playerWinCtr, short computerWinCtr, short drawCtr) {
+
+	printHead();
+	cout << "\n\t\tGame Rounds          : " << roundNumber;
+	cout << "\n\t\tPlayer Won Times     : " << playerWinCtr;
+	cout << "\n\t\tComputer Won Times   : " << computerWinCtr;
+	cout << "\n\t\tDraw Tiems           : " << drawCtr;
+	cout << "\n\t\tFinal Winner         : ";
+	checkFinalWinner(playerWinCtr, computerWinCtr, drawCtr);
+
+	cout << "\n\t\t__________________________________________________________________________________________";
+}
+
+void repeatGame(char &repeat) {
+	cout << "\n\t\tDo you want to play again? Y/N? ";
+	cin >> repeat;
+	cout << endl;
+}
+
+void startGame() {
+	char repeat;
+
+	do {
+		system("cls");
+		system("color 0F");
+		short roundNumber = roundNumberFunc();
+		short ctrRound = 0, playerWinCtr = 0, computerWinCtr = 0, drawCtr = 0;
+
+		while (roundNumber != ctrRound) {
+			++ctrRound;
+			short playerChoice = playerChoiceFunc(ctrRound);
+			short computerChoice = computerChoiceFunc(1, 3);
+			short roundWinner = roundWinnerFunc(playerChoice, computerChoice, playerWinCtr, computerWinCtr, drawCtr);
+			printRoundResult(ctrRound, playerChoice, computerChoice, roundWinner);
+		}
+		printFinalResult(roundNumber, playerWinCtr, computerWinCtr, drawCtr);
+		repeatGame(repeat);
+	} while (repeat == 'Y' || repeat == 'y');
+}
+
+int main()
+{
+	srand(unsigned(time(NULL)));
+
+	startGame();
+
+	return 0;
+}
---
+++ 05. Algorithms & Problem Solving (L2)/05 Course - Project 2.cpp
@@ -0,0 +1,192 @@
+﻿/* Test Your Math Skill Program */
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// Define difficulty levels as an enumeration
+enum enQuestionLevel { easy = 1, midium = 2, hard = 3, mixLevel = 4 };
+// Define mathematical operations as an enumeration
+enum enOperation { add = 1, sub = 2, mul = 3, divi = 4, mixOp = 5 };
+
+// Structure to store program configuration from user
+struct stProgramInfo {
+    short QuestionsNumber;          // Number of questions user wants
+    short QuestionsLevelInput;      // Difficulty level choice
+    short QusetionOperationInput;   // Operation type choice
+};
+
+// Collect user preferences for the quiz
+stProgramInfo getInfo() {
+    stProgramInfo userInput;
+    cout << " How many question do you want? ";
+    cin >> userInput.QuestionsNumber;
+    cout << " Enter question level [easy = 1, midium = 2, hard = 3, mixLevel = 4]: ";
+    cin >> userInput.QuestionsLevelInput;
+    cout << " Enter question operation [add = 1, sub = 2, mul = 3, div = 4, mixOp = 5]: ";
+    cin >> userInput.QusetionOperationInput;
+    cout << endl;
+
+    return userInput;
+}
+
+// Generate random number within specified range
+int randomNum(short from, short to) {
+    return rand() % (to - from + 1) + from;
+}
+
+// Generate random number based on selected difficulty level
+short getRandomNum(stProgramInfo ProgramInfo) {
+    switch (ProgramInfo.QuestionsLevelInput) {
+    case enQuestionLevel::easy:
+        return randomNum(0, 9);      // Easy: numbers 0-9
+    case enQuestionLevel::midium:
+        return randomNum(10, 99);    // Medium: numbers 10-99
+    case enQuestionLevel::hard:
+        return randomNum(100, 999);  // Hard: numbers 100-999
+    default:
+        return randomNum(0, 150);    // Mix: numbers 0-150
+    }
+}
+
+// Get operation symbol based on user choice
+char getOperation(stProgramInfo ProgramInfo) {
+    switch (ProgramInfo.QusetionOperationInput) {
+    case enOperation::add:
+        return '+';   // Addition
+    case enOperation::sub:
+        return '-';   // Subtraction
+    case enOperation::mul:
+        return '*';   // Multiplication
+    case enOperation::divi:
+        return '/';   // Division
+    default:
+        // For mixOp, generate random operation symbol from ASCII 42-47
+        // (excluding comma and period which are ASCII 44 and 46)
+        int RightOp = randomNum(42, 47);
+        while (RightOp == 44 || RightOp == 46)
+            RightOp = randomNum(42, 47);
+        return (char)RightOp;
+    }
+}
+
+// Calculate result for mixed operation using ASCII value
+int mixOperationResult(int n1, int n2, char charOperation) {
+    int AsciiOperation = (int)charOperation; // Convert char to ASCII code
+    switch (AsciiOperation) {
+    case 42:  // ASCII 42 = '*'
+        return n1 * n2;
+    case 43:  // ASCII 43 = '+'
+        return n1 + n2;
+    case 45:  // ASCII 45 = '-'
+        return n1 - n2;
+    default:  // ASCII 47 = '/'
+        return n1 / n2;
+    }
+}
+
+// Calculate correct answer for given operation
+short getResult_N1_Op_N2(stProgramInfo Opertaion, int n1, int n2, char charOperation) {
+    switch (Opertaion.QusetionOperationInput) {
+    case enOperation::add:
+        return n1 + n2;
+    case enOperation::sub:
+        return n1 - n2;
+    case enOperation::mul:
+        return n1 * n2;
+    case enOperation::divi:
+        if (n2 == 0) return n1 / 1;  // Avoid division by zero
+        return n1 / n2;
+    default:
+        return mixOperationResult(n1, n2, charOperation);
+    }
+}
+
+// Check if user's answer is correct and update score
+void checkAnswer(int answer, int number1, int number2, int& RightAnswerCtr, int& WrongAnswerCtr, stProgramInfo ProgramInfo, char charOperation) {
+    int result = getResult_N1_Op_N2(ProgramInfo, number1, number2, charOperation);
+
+    if (answer == result) {
+        cout << "\nRight answer.";
+        system("color 2F");  // Green background for correct answer
+        RightAnswerCtr++;
+    }
+    else {
+        cout << "\nWrong answer.";
+        system("color 4F");  // Red background for wrong answer
+        WrongAnswerCtr++;
+    }
+}
+
+// Convert level choice number to descriptive string
+string QuestionsLevel(short choice) {
+    string arrQuestionsLevel[4] = { "easy", "midium", "hard", "mix level" };
+    return arrQuestionsLevel[choice - 1];
+}
+
+// Convert operation choice number to descriptive string
+string QuestionsOperation(short choice) {
+    string arrQuestionsOperation[5] = { "addition", "substract", "multiplication", "division", "mix operation" };
+    return arrQuestionsOperation[choice - 1];
+}
+
+// Display final results summary
+void printOverScreen(stProgramInfo ProgramInfo, int Rctr, int Wctr) {
+    cout << "\n\n_______________________\n";
+    cout << "\n Final Result Pass :-) ";
+    cout << "\n______________________\n";
+    cout << "\nNumber of question: " << ProgramInfo.QuestionsNumber;
+    cout << "\nLeve of question: " << QuestionsLevel(ProgramInfo.QuestionsLevelInput);
+    cout << "\nOpertation of question: " << QuestionsOperation(ProgramInfo.QusetionOperationInput);
+    cout << "\nRight answer: " << Rctr;
+    cout << "\nWrong ansewr: " << Wctr;
+    cout << "\n_______________________";
+}
+
+// Main quiz execution - generates questions and collects answers
+void showProgramScreen(stProgramInfo ProgramInfo) {
+    int N1 = 0, N2 = 0, Answer = 0, Rctr = 0, Wctr = 0;
+    char Operation;
+
+    for (int i = 1; i <= ProgramInfo.QuestionsNumber; i++) {
+        N1 = getRandomNum(ProgramInfo), N2 = getRandomNum(ProgramInfo);
+        Operation = getOperation(ProgramInfo);
+        if (N2 > N1) swap(N1, N2);  // Ensure larger number first for subtraction/division
+        cout << "\n\n Question [" << i << "/" << ProgramInfo.QuestionsNumber << "]\n";
+        cout << N1 << '\n' << N2 << '\t' << Operation << '\n' << "__________\n";
+        cin >> Answer;
+        checkAnswer(Answer, N1, N2, Rctr, Wctr, ProgramInfo, Operation);
+    }
+
+    printOverScreen(ProgramInfo, Rctr, Wctr);
+}
+
+// Clear screen and reset colors for new game
+void resetScreen() {
+    system("cls");
+    system("color 0F");
+}
+
+// Main program loop with replay option
+void startProgram() {
+    char repeat = 'Y';
+    do {
+        resetScreen();
+        showProgramScreen(getInfo());
+
+        cout << "\n\nDo you want to play again? ";
+        cin >> repeat;
+
+    } while (repeat == 'y' || repeat == 'Y');
+}
+
+// Entry point - seed random generator and start program
+int main()
+{
+    srand(unsigned(time(NULL)));  // Initialize random seed
+
+    startProgram();
+
+    return 0;
+}
