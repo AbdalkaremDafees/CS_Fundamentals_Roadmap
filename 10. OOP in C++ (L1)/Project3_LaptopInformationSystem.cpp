7cf0d3022fb26d35c25d86430281f2c489b5ea44
Abdalkarem Dafees <karimhakimak24@gmail.com>
2026-03-07 20:52:22 +0300

    adding friend classes concept

--- 10. OOP in C++ (L1)/Project3_LaptopInformationSystem.cpp
+++ 10. OOP in C++ (L1)/Project3_LaptopInformationSystem.cpp
@@ -1,6 +1,6 @@
 /*
-            Project 3: simple laptop information system using
-                        abstract interface concept    
+            Project 3: adding friend classes concept to
+          manage basic and detailed laptop specifications    
 */
 #include <iostream>
 using namespace std;
@@ -44,15 +44,47 @@ public:
         return _LaptopPrice;
     }
 
+    // Allows clsLaptopSpecifications to access private members
+    friend class clsLaptopSpecifications;
+};
+
+// Extends basic info with detailed specifications (weight, warranty, color)
+class clsLaptopSpecifications : public clsLaptopStandardInfo
+{
+private:
+    float  _LaptopWeight;
+    int _LaptopWarranty;
+    string _LaptopColor;
+
+public:
+    clsLaptopSpecifications(string LaptopName, string LaptopCode, float LaptopPrice,
+        float LaptopWeight, int LaptopWarranty, string LaptopColor)
+        : clsLaptopStandardInfo(LaptopName, LaptopCode, LaptopPrice)
+    {
+        _LaptopWeight = LaptopWeight;
+        _LaptopWarranty = LaptopWarranty;
+        _LaptopColor = LaptopColor;
+    }
+
+    // Displays complete laptop information including all specifications
+    void Print()
+    {
+        cout << "========Laptop Standard Info========";
+        cout << "\nLaptop Name : " << _LaptopName;
+        cout << "\nLaptop Code : " << _LaptopCode;
+        cout << "\nLaptop Price: " << _LaptopPrice << '$';
+        cout << "\n\n========Laptop Specifications========";
+        cout << "\nLaptop Weight  : " << _LaptopWeight << " Kg";
+        cout << "\nLaptop Warranty: " << _LaptopWarranty << " Years";
+        cout << "\nLaptop Color   : " << _LaptopColor;
+    }
 };
 
 int main()
 {
     // Create and display a sample laptop
-    clsLaptopStandardInfo Laptop1("Lenovo ThinkPad", "T560", 850.5);
-    cout << "\nLaptop Name:  " << Laptop1.LaptopName();
-    cout << "\nLaptop Code:  " << Laptop1.LaptopCode();
-    cout << "\nLaptop Price: " << Laptop1.LaptopPrice() << '$';
+    clsLaptopSpecifications Laptop1("Lenovo ThinkPad", "T560", 850.5, 1.8, 2, "Gray");
+    Laptop1.Print();
 
     return 0;
 }
