3fa22d5dc5c7296028cb337dfd62a4ddb679c56c
Abdalkarem Dafees <karimhakimak24@gmail.com>
2026-03-08 14:23:10 +0300

    Adding nested class concept

--- 10. OOP in C++ (L1)/Project3_LaptopInformationSystem.cpp
+++ 10. OOP in C++ (L1)/Project3_LaptopInformationSystem.cpp
@@ -1,6 +1,8 @@
 /*
-            Project 3: adding friend classes concept to
-          manage basic and detailed laptop specifications    
+    Project 3: adding nested classes concept to
+    manage basic and detailed laptop specifications
+    This program demonstrates nested classes and friend classes
+    for laptop information with discount calculation
 */
 #include <iostream>
 using namespace std;
@@ -46,6 +48,7 @@ public:
 
     // Allows clsLaptopSpecifications to access private members
     friend class clsLaptopSpecifications;
+    friend class clsDiscount;
 };
 
 // Extends basic info with detailed specifications (weight, warranty, color)
@@ -53,17 +56,48 @@ class clsLaptopSpecifications : public clsLaptopStandardInfo
 {
 private:
     float  _LaptopWeight;
-    int _LaptopWarranty;
+    int    _LaptopWarranty;
     string _LaptopColor;
+    float  _DiscountAmount;
 
 public:
+    // Nested class for discount functionality
+    // This class is defined inside clsLaptopSpecifications to show nested class concept
+    class clsDiscount
+    {
+    private:
+        float _DiscountAmount;
+        float _LaptopPrice;
+        float _Result;
+
+    public:
+        clsDiscount(float LaptopPrice, float DiscountAmount = 0)
+        {
+            _LaptopPrice = LaptopPrice;
+            _DiscountAmount = DiscountAmount;
+            _Result = _LaptopPrice - _DiscountAmount;
+        }
+
+        // Displays the discounted price
+        void PrintDiscount()
+        {
+            cout << "\n\nThe price after discount " << _DiscountAmount << '$'
+                 << " is: " << _Result << '$';
+        }
+    };
+
+    // Nested class object as member
+    clsDiscount Discount;
+
     clsLaptopSpecifications(string LaptopName, string LaptopCode, float LaptopPrice,
-        float LaptopWeight, int LaptopWarranty, string LaptopColor)
-        : clsLaptopStandardInfo(LaptopName, LaptopCode, LaptopPrice)
+        float LaptopWeight, int LaptopWarranty, string LaptopColor, float DiscountAmount = 0)
+        : clsLaptopStandardInfo(LaptopName, LaptopCode, LaptopPrice), 
+          Discount(LaptopPrice, DiscountAmount)  // Initialize nested object in member initializer list
     {
         _LaptopWeight = LaptopWeight;
         _LaptopWarranty = LaptopWarranty;
         _LaptopColor = LaptopColor;
+        _DiscountAmount = DiscountAmount;
     }
 
     // Displays complete laptop information including all specifications
@@ -82,9 +116,19 @@ public:
 
 int main()
 {
-    // Create and display a sample laptop
-    clsLaptopSpecifications Laptop1("Lenovo ThinkPad", "T560", 850.5, 1.8, 2, "Gray");
+    // Create and display a sample laptop with discount
+    clsLaptopSpecifications Laptop1("Lenovo ThinkPad", "T560", 850.5, 1.8, 2, "Gray", 45.5);
     Laptop1.Print();
+    
+    // Access nested class method through the outer class object
+    Laptop1.Discount.PrintDiscount();
+
+    cout << "\n\n";
+
+    // Create another laptop with different specifications
+    clsLaptopSpecifications Laptop2("Dell XPS", "XPS13", 1200.0, 1.2, 3, "Silver", 100.0);
+    Laptop2.Print();
+    Laptop2.Discount.PrintDiscount();
 
     return 0;
 }
