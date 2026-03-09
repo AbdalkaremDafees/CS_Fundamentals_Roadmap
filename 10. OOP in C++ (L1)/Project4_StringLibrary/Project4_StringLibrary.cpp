309fbb8009c14279b4f38a75b4d7f112bfc85146
Abdalkarem Dafees <karimhakimak24@gmail.com>
2026-03-09 23:14:47 +0300

    Start project 4: string manipulation library using OOP principles.

---
+++ 10. OOP in C++ (L1)/Project4_StringLibrary/Project4_StringLibrary.cpp
@@ -0,0 +1,21 @@
+﻿/*
+            Project 4: String Manipulation Library 
+       A Dual-Approach Word Counter Using OOP Principles
+*/
+#include <iostream>
+#include "clsString.h"
+using namespace std;
+
+int main()
+{
+    clsString String1;
+    clsString String2("Abdalkarem");
+
+    String1.Value = "Mohammed";
+
+    cout << "String1 = " << String1.Value << endl;
+    cout << "String2 = " << String2.Value << endl;
+    
+    system("pause>0");
+    return 0;
+}
---
+++ 10. OOP in C++ (L1)/Project4_StringLibrary/clsString.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+class clsString
+{
+
+private:
+	string _Value;
+
+public:
+	clsString()
+	{
+		_Value = " ";
+	}
+
+	clsString(string Value)
+	{
+		_Value = Value;
+	}
+
+	void SetValue(string Value)
+	{
+		_Value = Value;
+	}
+
+	string GetValue()
+	{
+		return _Value;
+	}
+
+	__declspec(property(get = GetValue, put = SetValue)) string Value;
+};
+
