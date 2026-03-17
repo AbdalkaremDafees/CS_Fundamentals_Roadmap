66f663bfc9b6267fa4beb498bfb1fa6afd5cf39c
Abdalkarem Dafees <karimhakimak24@gmail.com>
2026-03-17 23:01:28 +0300

    Add the main cpp file

---
+++ 10. OOP in C++ (L1)/Project5_DateLibrary/Project5_DateLibrary.cpp
@@ -0,0 +1,22 @@
+﻿/*
+        Project 5: Date Library, A complete date handling library
+            with multiple constructors and utility functions
+*/
+#include <iostream>
+#include "clsDate.h";
+using namespace std;
+
+int main()
+{
+    clsDate Date1;
+    Date1.Print();
+
+    clsDate Date2("31/1/2022");
+    Date2.Print();
+
+    clsDate Date3(20, 12, 2022);
+    Date3.Print();
+
+    system("pause 0");
+    return 0;
+}
