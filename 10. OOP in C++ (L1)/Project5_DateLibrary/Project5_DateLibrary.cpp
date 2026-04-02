8f68744aecc73b09eb632984f13bd8c8fe1bd4e0
Abdalkarem Dafees <karimhakimak24@gmail.com>
2026-04-02 21:49:52 +0300

    Add main.cpp with entry point and test implementation for date utility functions

--- 10. OOP in C++ (L1)/Project5_DateLibrary/Project5_DateLibrary.cpp
+++ 10. OOP in C++ (L1)/Project5_DateLibrary/Project5_DateLibrary.cpp
@@ -9,21 +9,29 @@ using namespace std;
 int main()
 {
     clsDate Date1;
+    cout << "Date 1: ";
     Date1.Print();
 
     clsDate Date2("31/1/2022");
+    cout << "Date 2: ";
     Date2.Print();
 
     clsDate Date3(20, 12, 2022);
+    cout << "Date 3: ";
     Date3.Print();
 
     clsDate Date4(250, 2022);
+    cout << "Date 4: ";
     Date4.Print();
 
     cout << "Is Date 4 Valid Date? " << clsDate::IsValidDate(Date4) << '\n';
 
-    cout << "Number Of Seconds In Year " << Date2.Year << ' ' << 
+    cout << "Number Of Seconds In Year " << Date2.Year << ": " << 
         Date2.NumberOfSecondsInAYear(Date2.Year) << " sec\n";
+
+    cout << "Day Short Name In Date 2: " << Date2.DayShortName() << '\n';
+
+    cout << "Month Short Name In Date 3: " << Date3.MonthShortName() << '\n';
 
     system("pause 0");
     return 0;
