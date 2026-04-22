85e449c6794a951700d8ffb176b1544253171a0e
Abdalkarem Dafees <karimhakimak24@gmail.com>
2026-04-22 20:26:58 +0300

    Start Project 1: Utility Library

---
+++ 11. OOP in C++ (L2)/Project 1 - Utility Library/Utility Library.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "clsUtility.h"
+using namespace std;
+
+int main()
+{
+    clsUtility::Srand();
+    cout << clsUtility::RandomNumber(1, 10) << '\n';
+    cout << clsUtility::GetRandomCharacter(clsUtility::MixChars) << '\n';
+    cout << clsUtility::GenerateWord(clsUtility::MixChars, 8) << '\n';
+    cout << clsUtility::GenerateKey(clsUtility::CapitalLetter) << '\n';
+    clsUtility::GenerateKeys(clsUtility::CapitalLetter, 10);
+
+    cout << endl;
+
+    return 0;
+}
