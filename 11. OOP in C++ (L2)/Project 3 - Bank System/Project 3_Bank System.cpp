6180002e7d4000c975f405c1d6638a11fd56beb5
AbdalkaremDafees <karimhakimak24@gmail.com>
2026-09-26 22:04:42 +0300

    Test the program in the main.

--- 11. OOP in C++ (L2)/Project 3 - Bank System/Project 3_Bank System.cpp
+++ 11. OOP in C++ (L2)/Project 3 - Bank System/Project 3_Bank System.cpp
@@ -1,61 +1,12 @@
 #include <iostream>
-#include <iomanip>
-#include "clsBankClient.h"
-#include "clsUtility.h"
-#include "clsInputValidate.h"
+#include "clsMainScreen.h"
 using namespace std;
-
-void PrintClientRecordLine(clsBankClient Client)
-{
-
-	cout << "| " << setw(15) << left << Client.AccountNumber();
-	cout << "| " << setw(20) << left << Client.FullName();
-	cout << "| " << setw(12) << left << Client.AccountBalance;
-
-}
-
-// To show the total balances with numbers.
-void ShowClientsList()
-{
-	vector<clsBankClient> vClients = clsBankClient::GetClientsList();
-
-    cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client(s).";
-    cout << "\n_______________________________________________________";
-    cout << "_________________________________________\n" << endl;
-
-    cout << "| " << left << setw(15) << "Accout Number";
-    cout << "| " << left << setw(20) << "Client Name";
-    cout << "| " << left << setw(12) << "Balance";
-    cout << "\n_______________________________________________________";
-    cout << "_________________________________________\n" << endl;
-
-    double TotalBalances = clsBankClient::GetTotalBalances();
-
-    if (vClients.size() == 0)
-    {
-        cout << "\t\t\t\tNo Clients Available In the System!";
-    }
-    else
-    {
-        for (clsBankClient Client : vClients)
-        {
-            PrintClientRecordLine(Client);
-            cout << endl;
-        }
-    }
-
-    cout << "\n_______________________________________________________";
-    cout << "_________________________________________\n" << endl;
-
-    cout << "\t\t\t\t\t   Total Balances = " << TotalBalances << endl;
-    cout << "\t\t\t\t\t   ( " << clsUtil::NumberToText(TotalBalances) << ")";
-
-}
 
 int main()
 {	
-    ShowClientsList();
+	clsMainScreen::ShowMainMenue();
 
+	system("pause>0");
 	return 0;
 }
 
